3263573fe7b0c20a6f58bb2c18a3ce0cd061e17d
Joseph <cutinoj@mail.gvsu.edu>
2017-12-11 10:39:32 -0500

    rx code sample

---
+++ serialRx.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+import serial
+import time
+ser = serial.Serial(
+   port = '/dev/ttyAMA0',
+   baudrate = 9600,
+   parity = serial.PARITY_NONE,
+   stopbits = serial.STOPBITS_ONE,
+   bytesize = serial.EIGHTBITS,
+   timeout = 1
+)
+while True:
+   print (('SERIAL PORT:'), ser.readline())
+   time.sleep(1)
