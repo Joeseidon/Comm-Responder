134516dfade8515d376db8df84250f3663604632
Joseph <cutinoj@mail.gvsu.edu>
2017-12-15 21:22:58 +0000

    attempted to fix comm issues

--- serialCommTester.py
+++ serialCommTester.py
@@ -47,7 +47,7 @@ class Op_Mode(Enum):
 '''
 ser = serial.Serial(
    port = '/dev/ttyAMA0',
-   baudrate = 9600,
+   baudrate = 250000,
    parity = serial.PARITY_NONE,
    stopbits = serial.STOPBITS_ONE,
    bytesize = serial.EIGHTBITS,
